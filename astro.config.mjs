f828bfcdc02b341be556c7e286284095d01f63ac
wayanad house <150238196+wayanadiitm@users.noreply.github.com>
2025-01-10 12:22:19 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://logicloom.iitmparadox.org',
-  //site: 'https://wayanadiitm.github.io',
-  //base: 'event-template-3',
+  //site: 'https://logicloom.iitmparadox.org',
+  site: 'https://wayanadiitm.github.io',
+  base: 'logicloom3',
   integrations: [mdx(), sitemap(), tailwind()]
 });
